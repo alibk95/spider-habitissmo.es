d7203743e507080b84b0c605e10cf2aca2ad385b
alibk95 <abkarimi1995@gmail.com>
2020-12-08 12:16:52 +0100

    structure implemented

--- codebase/__init__.py
+++ codebase/__init__.py
@@ -3,8 +3,32 @@ from bs4 import BeautifulSoup
 
 
 
+class Spider:
+    def __init__(self):
+        self.article = str()
+        self.base_url = "https://empresas.habitissimo.es/"
 
-url = 'https://empresas.habitissimo.es/do_ajax/business_modal_phone'
+    @staticmethod
+    def get_highlevel_categories() -> list:
+        return ['construccion', 'reformas', 'mudanzas', 'obras-menores', 'instaladores', 'mantenimiento', 'tiendas']
+
+    @staticmethod
+    def build_highlevel_urls(self):
+        categories = Spider().get_highlevel_categories()
+        url_list = []
+        for cat in categories:
+            url_list.append(self.base_url+cat)
+        return url_list
+
+    
+
+
+if __name__ == '__main__':
+    spider = Spider()
+    print(spider.build_highlevel_urls())
+
+
+# url = 'https://empresas.habitissimo.es/do_ajax/business_modal_phone'
 
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
